4ef6e8acc9b6a24badbfb6c012d5f6fdfa006be7
Dang Nhat Minh <dangnhatminh09032002@gmail.com>
2024-08-07 19:04:54 +0000

    chore: update dependencies and add Prettier configuration files

---
+++ apps/payment-service/src/services/identity.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common';
+
+import {
+  GetUsers200ResponseOneOfInner,
+  ManagementClient,
+  UserInfoClient,
+} from 'auth0';
+
+export const management = new ManagementClient({
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_MANAGEMENT_ID,
+  clientSecret: process.env.AUTH0_MANAGEMENT_SECRET,
+});
+
+export const userInfo = new UserInfoClient({
+  domain: process.env.AUTH0_DOMAIN,
+});
+
+@Injectable()
+export class IdentityService {
+  constructor() {}
+
+  async validate(token: string) {
+    return userInfo.getUserInfo(token).then((res) => ({
+      sub: res.data.sub,
+      user_id: res.data.sub,
+      email: res.data.email,
+      name: res.data.name,
+      picture: res.data.picture,
+    }));
+  }
+
+  async getAll(): Promise<any> {
+    return await management.users.getAll();
+  }
+
+  async listUsers(ids: string[]): Promise<
+    Array<{
+      user_id: string;
+      email: string;
+      name: string;
+      picture: string;
+    }>
+  > {
+    if (ids.length === 0) return [];
+
+    const allUsers: GetUsers200ResponseOneOfInner[] = [];
+    const queryIds = ids.map((id) => `user_id:${id}`).join(' OR ');
+    let page = 0;
+    while (true) {
+      const {
+        data: { users, total },
+      } = await management.users.getAll({
+        include_totals: true,
+        page: page++,
+        q: queryIds,
+      });
+      allUsers.push(...users);
+      if (allUsers.length === total) break;
+    }
+
+    return allUsers
+      .filter((user) => ids.includes(user.user_id))
+      .map((user) => ({
+        user_id: user.user_id,
+        email: user.email,
+        name: user.name,
+        picture: user.picture,
+      }));
+  }
+}
--- apps/payment-service/src/services/index.ts
+++ apps/payment-service/src/services/index.ts
@@ -1,16 +1,16 @@
 import { IdempotentService } from './idempotent.service';
 import { PaymentService } from './payment.service';
 import { RedisService } from './redis.service';
-import { UserService } from './user.service';
+import { IdentityService } from './identity.service';
 
 export * from './idempotent.service';
 export * from './payment.service';
 export * from './redis.service';
-export * from './user.service';
+export * from './identity.service';
 
 export const services = [
   IdempotentService,
+  IdentityService,
   RedisService,
   PaymentService,
-  UserService,
 ];
