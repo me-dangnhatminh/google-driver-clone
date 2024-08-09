f8caf100c311e48fe6cb08b0d7cdc419b67133b2
Dang Nhat Minh <dangnhatminh09032002@gmail.com>
2024-08-09 14:23:38 +0000

    chore: remove unused files and dependencies

--- apps/indentity-service/src/app.module.ts
+++ apps/indentity-service/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HTTPController, TCPController } from './api';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      expandVariables: true,
+      isGlobal: true,
+    }),
+  ],
+  controllers: [HTTPController, TCPController],
+  providers: [],
 })
 export class AppModule {}
--- apps/indentity-service/src/main.ts
+++ apps/indentity-service/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { auth } from 'express-openid-connect';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL;
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const AUTH0_SECRET = process.env.AUTH0_SECRET;
+console.log('BASE_URL', BASE_URL);
+
+const auth0Middleware = auth({
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: `${BASE_URL}`,
+  secret: AUTH0_SECRET,
+  clientID: AUTH0_CLIENT_ID,
+  issuerBaseURL: `https://${AUTH0_DOMAIN}`,
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(auth0Middleware);
+
+  app.enableCors({
+    origin: /http:\/\/localhost/,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(PORT, () => {
+    Logger.log(`Server is running on http://localhost:${PORT}`, 'Bootstrap');
+  });
 }
 bootstrap();
